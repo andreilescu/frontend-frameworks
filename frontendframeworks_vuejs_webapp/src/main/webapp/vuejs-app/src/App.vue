acbbd7c7b8379f4b6a3a225ebff292506a03846f
andrei.lescu <andrei.lescu@edeka-suedwest.de>
2018-08-18 10:59:43 +0200

    Fixed typo in module name. Temp commit.

--- frontendframeworks_vuejs_webapp/src/main/webapp/vuejs-app/src/App.vue
+++ frontendframeworks_vuejs_webapp/src/main/webapp/vuejs-app/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <list-note/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import ListNote from './components/ListNote.vue'
 
-export default {
+    export default {
   name: 'app',
   components: {
-    HelloWorld
+      ListNote
   }
 }
 </script>
---
+++ frontendframeworks_vuejs_webapp/src/main/webapp/vuejs-app/src/components/ListNote.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hello">
+
+        <input v-model="note.title"/>
+        <input v-model="note.description"/>
+
+        <button v-on:click="bar()">Add note</button>
+
+        <div v-for="note in notes">
+            <span>{{note.title}}</span>
+            <span>{{note.description}}</span>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'ListNote',
+        methods: {
+            bar() {
+                this.notes.push(this.note);
+                this.note = {};
+            }
+        },
+        data: function() {
+            return {
+                notes: [],
+                note: {
+                    title: '',
+                    description: ''
+                }
+            }
+        }
+    }
+
+</script>
+
+
